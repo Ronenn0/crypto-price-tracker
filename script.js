ae9821201afbb0334c796c833a249f0a1db79eee
Ronenn0 <gunronen@gmail.com>
2025-08-29 01:05:09 +0300

    1. Add Cache -> check cache before fetching the API. 2. Save Last input and load it on load.

--- script.js
+++ script.js
@@ -11,29 +11,46 @@ const coinImage = document.querySelector('.coin-image');
 
 const coinSelectElement = document.querySelector('.coin-select');
 
+let cache;
+
 /**
  * gets the price of the inputted coin or displays Error message
+ * @param {boolean} coolDown -> should it coolDown after rendering the result or not.
  */
-async function checkPrice(firstTime) {
+async function checkPrice(coolDown = true) {
     if (serviceDown) return;
     const coinName = cryptoInput.value.toLowerCase();
     if (lastChecked == coinName) return;
     lastChecked = coinName;
     showSpinner();
     try {
-        const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinName}`);
-        const data = await response.json();
         let price = 'Coin not found';
         let imageSrc = '';
-        if (data.market_data) {
-            price = data.market_data.current_price.usd.toLocaleString() + '$';
-            imageSrc = data.image.large;
+
+        //Check saved cache.
+        if (cache[coinName]) {
+            price = cache[coinName].price;
+            imageSrc = cache[coinName].imageSrc;
+            coolDown = false;
+        } else {
+            const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinName}`);
+            const data = await response.json();
+
+            if (data.market_data) {
+                price = data.market_data.current_price.usd.toLocaleString() + '$';
+                imageSrc = data.image.large;
+                cache[coinName] = {
+                    price, imageSrc
+                };
+                saveCache();
+            }
         }
+
         displayData(coinName, price, imageSrc);
     } catch (error) {
         displayData(coinName, 'Something went wrong.', '');
     }
-    if (firstTime) return;
+    if (!coolDown) return;
     serviceDown = true;
     setTimeout(() => {
         serviceDown = false;
@@ -49,6 +66,12 @@ async function checkPrice(firstTime) {
     checkButton.value = 'Check Price';
 }
 
+/**
+ * 
+ * @param {number} ms -> milliseconds 
+ * @returns a promise that needs (ms) milliseconds to be done.
+ * the point is to sleep ms milleseconds.
+ */
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -59,10 +82,16 @@ function sleep(ms) {
  * @param {string} response -> API response (price) or Error message
  */
 function displayData(name, response, imageSrc) {
+    cryptoInput.value = name;
     coinNameElement.innerHTML = name;
     coinPriceElement.innerHTML = response;
     coinImage.style.display = imageSrc == '' ? 'none' : 'block';
     coinImage.src = imageSrc;
+    const lastCoin = {
+        name, response, imageSrc
+    };
+    //saves last inputted coin
+    localStorage.setItem('lastCoin', JSON.stringify(lastCoin));
 }
 
 /**
@@ -82,6 +111,10 @@ function showSpinner() {
 function addEvents() {
     checkButton.addEventListener('click', checkPrice);
     coinSelectElement.addEventListener('input', autoCheckCurrency);
+
+    //prevents sending the form
+    const coinForm = document.querySelector('.coin-form');
+    coinForm.addEventListener('submit', e => e.preventDefault());
 }
 
 /**
@@ -94,9 +127,51 @@ function autoCheckCurrency() {
     checkPrice();
 }
 
-function start() {
-    addEvents();
-    checkPrice(true);
+/**
+ * 
+ * loads the saved cache from the localStorage
+ */
+function loadCache() {
+    let savedCache = localStorage.getItem('cache');
+    if (savedCache) {
+        cache = JSON.parse(savedCache);
+        return;
+    }
+    cache = {};
+    saveCache();
 }
 
+/**
+ * Saves the cache to localStorage
+ */
+function saveCache() {
+    localStorage.setItem('cache', JSON.stringify(cache));
+}
+
+/**
+ * 
+ * @returns the last searched coin - from localStorage
+ */
+function getLastInputtedCoin() {
+    const lastIcon = localStorage.getItem('lastCoin');
+    if (lastIcon) {
+        return JSON.parse(lastIcon);
+    }
+}
+
+/**
+ * Initializing.
+ */
+function start() {
+    loadCache();
+    addEvents();
+    const lastIcon = getLastInputtedCoin();
+    if (lastIcon) {
+        displayData(lastIcon.name, lastIcon.response, lastIcon.imageSrc);
+    } else {
+        checkPrice(false);
+    }
+}
+
+//initializes the script.
 start();
