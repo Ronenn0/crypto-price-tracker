56774d327e3d974675c5a143bbc1a5415c5c8839
Ronenn0 <gunronen@gmail.com>
2025-08-30 09:10:13 +0300

    fixed an error

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function addEvents() {
  * (popular currencies).
  */
 function autoCheckCurrency() {
-    if (coolDown) return;
+    if (serviceDown) return;
     const currencyName = coinSelectElement.value;
     cryptoInput.value = currencyName;
     checkPrice();
